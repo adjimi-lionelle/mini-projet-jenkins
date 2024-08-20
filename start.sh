88283d8130a5cfd97667f2b7a1525cb446441f61
adjimi-lionelle <62795655+adjimi-lionelle@users.noreply.github.com>
2024-08-20 23:20:44 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# Remplace le placeholder dans le fichier de configuration avec la variable d'environnement LISTEN_PORT
 sed -i "s/LISTEN_PORT/${LISTEN_PORT}/g" /etc/nginx/nginx.conf
-
-# Démarre Nginx
 nginx -g 'daemon off;'
