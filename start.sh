5c1a24b3750cf3f591b575d024f8ebcfd5080204
adjimi-lionelle <62795655+adjimi-lionelle@users.noreply.github.com>
2024-08-26 12:50:41 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-if [ -z "$LISTEN_PORT" ]; then
-  PORT=80
-fi
-sed -i "s/LISTEN_PORT/${PORT}/g" /etc/nginx/nginx.conf
+sed -i "s/LISTEN_PORT/${LISTEN_PORT}/g" /etc/nginx/nginx.conf
 nginx -g 'daemon off;'
